06294230d3407e2f2c566e8ad029ba97ea4e583e
InigoTeope <127967223+InigoTeope@users.noreply.github.com>
2023-05-19 15:57:52 +0800

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,32 +1,43 @@
 import streamlit as st
 import tensorflow as tf
-
-@st.cache_resource
-def load_model():
-  model=tf.keras.models.load_model('final_model_best.h5')
-  return model
-model=load_model()
-st.write("""
-# Clothes Detection System by Iñigo Teope"""
-)
-file=st.file_uploader("Choose clothes photo from computer",type=["jpg","png"])
-
-import cv2
-from PIL import Image,ImageOps
 import numpy as np
-def import_and_predict(image_data,model):
-    size=(28,28)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
+from PIL import Image
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+# Define the class names
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+@st.cache(allow_output_mutation=True)
+def load_model(model_path):
+    model = tf.keras.models.load_model(final_model_best.h5)
+    return model
+
+def predict_image_class(image_path, model_path):
+    # Load the trained model
+    model = load_model(final_model_best.h5)
+
+    # Load the image and preprocess it
+    img = load_img(image_path, color_mode='grayscale', target_size=(28, 28))
+    img_array = img_to_array(img)
+    img_array = img_array.reshape((1, 28, 28, 1))
+    img_array = img_array.astype('float32')
+    img_array /= 255.0
+
+    # Make predictions
+    predictions = model.predict(img_array)
+    predicted_class = np.argmax(predictions, axis=1)[0]
+    predicted_class_name = class_names[predicted_class]
+
+    return predicted_class_name
+
+st.write("# Clothes Detection System by Iñigo Teope")
+
+file = st.file_uploader("Choose clothes photo from computer", type=["jpg", "png"])
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
-    st.success(string)
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predicted_class_name = predict_image_class(file, 'final_model_best.h5')
+    st.success("OUTPUT: " + predicted_class_name)
