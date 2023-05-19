5bf309ac3baf63592de36574abd1cb34b6c07595
InigoTeope <127967223+InigoTeope@users.noreply.github.com>
2023-05-19 15:46:41 +0800

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,33 +1,33 @@
 import streamlit as st
 import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('final_model_best.h5')
-  return model
-model=load_model()
-st.write("""
-# Clothes Classification System"""
-)
-file=st.file_uploader("Select a image that would contain a type of apparel",type=["jpg","png"])
+    model = tf.keras.models.load('final_model_best.h5')
+    return model
 
+model = load_model()
 
-import cv2
-from PIL import Image,ImageOps
-import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+st.write("# Clothes Classification System")
+
+file = st.file_uploader("Select an image containing a type of apparel", type=["jpg", "png"])
+
+def import_and_predict(image_data, model):
+    size = (64, 64)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
     return prediction
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    string = "OUTPUT: " + class_names[np.argmax(prediction)]
     st.success(string)
